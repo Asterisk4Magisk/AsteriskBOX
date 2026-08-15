6763b10ee77ff969714965e14d5401300ee60a28
Whale Choi <cj1369636717@gmail.com>
2026-08-15 10:14:52 +0800

    refactor: replace asteriskd startup polling with watch events

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -13,7 +13,7 @@ object ProjectConfig {
     const val PROJECT_NAME = "AsteriskBOX"
     const val VERSION_NAME = "1.1.0-dev"
     const val PACKAGE_NAME = "org.asterisk.zcc.abox"
-    const val ASTERISKD_VERSION = "v2.0.2"
+    const val ASTERISKD_VERSION = "v2.0.3"
     const val BPF2SOCKS_VERSION = "v1.0.4"
     const val BPF_MATCHER_VERSION = "v1.0.1"
     const val ANDROID_LIB_BOX_LITE_VERSION = "v1.14.0-beta.14-reF1nd"
