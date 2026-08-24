c57bc6a7a47cb7fb85cad5ae02a83018bdc5dfdd
Whale Choi <cj1369636717@gmail.com>
2026-08-25 05:58:17 +0800

    chore: bump dependencies version

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -13,7 +13,7 @@ object ProjectConfig {
     const val PROJECT_NAME = "AsteriskBOX"
     const val VERSION_NAME = "1.1.4-dev"
     const val PACKAGE_NAME = "org.asterisk.zcc.abox"
-    const val ASTERISKD_VERSION = "v2.0.18"
+    const val ASTERISKD_VERSION = "v2.0.19"
     const val BPF2SOCKS_VERSION = "v1.0.6"
     const val BPF_MATCHER_VERSION = "v1.0.1"
     const val ANDROID_LIB_BOX_LITE_VERSION = "v1.14.0-rc.1-reF1nd"
