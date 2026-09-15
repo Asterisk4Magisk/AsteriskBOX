c44c8051b0370ad8c51670303aad91b32266830d
Whale Choi <cj1369636717@gmail.com>
2026-09-16 00:40:49 +0800

    fix: update native modules for UDP socket compatibility

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -13,8 +13,8 @@ object ProjectConfig {
     const val PROJECT_NAME = "AsteriskBOX"
     const val VERSION_NAME = "1.1.9-dev"
     const val PACKAGE_NAME = "org.asterisk.zcc.abox"
-    const val ASTERISKD_VERSION = "v2.0.31"
-    const val BPF2SOCKS_VERSION = "v1.0.14"
+    const val ASTERISKD_VERSION = "v2.0.32"
+    const val BPF2SOCKS_VERSION = "v1.0.15"
     const val BPF_MATCHER_VERSION = "v1.0.1"
     const val ANDROID_LIB_BOX_LITE_VERSION = "v1.15.0-alpha.3-reF1nd"
     const val SING_BOX_VERSION = ANDROID_LIB_BOX_LITE_VERSION
