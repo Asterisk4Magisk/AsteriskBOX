a411ad29373cd00711b0577444c82eb5278398a6
Whale Choi <cj1369636717@gmail.com>
2026-09-16 01:56:46 +0800

    chore: bump sing-box version to v1.15.0-alpha.4-reF1nd

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -16,7 +16,7 @@ object ProjectConfig {
     const val ASTERISKD_VERSION = "v2.0.32"
     const val BPF2SOCKS_VERSION = "v1.0.15"
     const val BPF_MATCHER_VERSION = "v1.0.1"
-    const val ANDROID_LIB_BOX_LITE_VERSION = "v1.15.0-alpha.3-reF1nd"
+    const val ANDROID_LIB_BOX_LITE_VERSION = "v1.15.0-alpha.4-reF1nd"
     const val SING_BOX_VERSION = ANDROID_LIB_BOX_LITE_VERSION
     const val HEV_SOCKS5_TUNNEL_VERSION = "2.17.1"
     const val TARGET_SDK = 37
